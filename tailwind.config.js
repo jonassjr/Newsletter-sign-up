1f9b9266630df91828294eacb4cf8b2386fd7693
Jonas Saraiva Jr <jonas.saraivajr@gmail.com>
2023-06-12 20:38:24 -0300

    feat: Setup ESLint, Prettier, and Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        'primary':'#FFF7F7',
+        'secondary':'#27DEBF',
+        'third-color':'#DADADA',
+        'background-color': '#204969'
+      }
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+  
+    },
+  },
+  plugins: [],
+}
+
