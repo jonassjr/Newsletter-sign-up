a5be3507338d28a856dc2e109e204343a65067ad
Jonas Saraiva Jr <jonas.saraivajr@gmail.com>
2023-06-14 20:53:09 -0300

    create:implement desktop aprouch

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,30 @@
 import Input from './components/Input'
 
-import { TickCircle } from 'iconsax-react'
+import { Button } from './components/Button'
+import ImageSection from './components/ImageSection'
+import FeatureList from './components/FeatureList'
 
 import mobileImg from './assets/mobile-img.svg'
-import { Button } from './components/Button'
+import desktopImg from './assets/desktop-img.svg'
+import HeaderSection from './components/HeaderSection'
 
 function App() {
   return (
-    <div>
-      <div>
-        <img src={mobileImg} alt="newsletter img" className="w-full mb-4" />
-      </div>
-      <div className="flex flex-col gap-6 font-poppins mx-4 xs:mx-auto xs:w-[280px]">
-        <div>
-          <h1
-            className="text-[32px] font-semibold
-           text-background-color"
-          >
-            Stay In the Loop!
-          </h1>
-          <p className="text-[14px] text-gray-700">
-            Be part of our community of 20,000+ subscribers and receive monthly updates on:
-          </p>
+    <div className="md:bg-background-color md:w-full h-screen flex md:items-center justify-center">
+      <section className="md:grid grid-cols-2 gap-x-8 md:bg-primary  md:place-items-center h-[570px] md:p-8 md:rounded-[35px]">
+        <ImageSection mobileImg={mobileImg} desktopImg={desktopImg} />
+
+        <div className="flex flex-col gap-6 font-poppins mx-4 xs:mx-auto xs:w-[280px] md:w-[402px]">
+          <HeaderSection />
+
+          <FeatureList />
+
+          <form action="" className="flex flex-col gap-y-3 mb-8 md:mb-0">
+            <Input />
+            <Button />
+          </form>
         </div>
-
-        <ul className="flex flex-col gap-2 ">
-          <li className="text-[14px] text-gray-700 flex gap-x-2">
-            <div>
-              <TickCircle className="text-secondary" />
-            </div>
-            <p>Discovering Valuable Products and Features</p>
-          </li>
-          <li className="text-[14px] text-gray-700 flex gap-x-2">
-            <div>
-              <TickCircle className="text-secondary" />
-            </div>
-            <p>Ensuring Successful Updates through Metrics</p>
-          </li>
-          <li className="text-[14px] text-gray-700 flex min-[283px]:items-center gap-x-2">
-            <div>
-              <TickCircle className="text-secondary " />
-            </div>
-            <p>And Many Exciting Topics Await</p>
-          </li>
-        </ul>
-
-        <form action="" className="flex flex-col gap-y-3 mb-8">
-          <Input />
-          <Button />
-        </form>
-      </div>
+      </section>
     </div>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,16 @@
 export function Button() {
+  const handleClick = (ev) => {
+    ev.preventDefault()
+  }
+
   return (
     <button
       id="sign-up-btn"
       type="submit"
-      className="py-4 text-[12px] bg-background-color 
-      rounded-[10px] font-poppins font-semibold text-primary max-w-[280px]"
+      className="py-4 text-[12px] md:text-[14px] bg-background-color 
+      rounded-[10px] font-poppins font-semibold text-primary max-w-[280px]
+      md:max-w-[402px] duration-500 hover:bg-[#285A82] hover:shadow-xl"
+      onClick={handleClick}
     >
       Receive Monthly Newsletter
     </button>
